b4118677c55720ad5b8151ee82bafc2a438b0ee9
zaftnotameni <mozair.carmo+zaft@gmail.com>
2024-08-02 13:24:12 +0200

    manipulating animation player libraries crashes godot, wonderful

--- mechanically-challenged/scripts/rewinder-animation-player.gd
+++ mechanically-challenged/scripts/rewinder-animation-player.gd
@@ -41,15 +41,22 @@ func create_overlay():
 
 func _unhandled_input(event: InputEvent) -> void:
   if event.is_action_pressed(INPUT_REWIND):
+    print_verbose('input rewind')
     start_rewind()
 
 func pause_and_grayout():
-  get_tree().paused = true
+  print_verbose('START pause_and_grayout')
+  # get_tree().paused = true
+  pause_rewinders()
   overlay_rect.color.a = 0.2
+  print_verbose('END pause_and_grayout')
 
 func unpause_and_clear():
+  print_verbose('START unpause_and_clear')
   overlay_rect.color.a = 0.0
-  get_tree().paused = false
+  # get_tree().paused = false
+  unpause_rewinders()
+  print_verbose('END unpause_and_clear')
 
 func on_animation_started(_animation_name:StringName):
   print_verbose('started rewinding')
@@ -57,30 +64,60 @@ func on_animation_started(_animation_name:StringName):
 func on_animation_finished(_animation_name:StringName):
   print_verbose('finished rewinding')
   clear_animation()
-  reset_rewinders()
-  unpause_and_clear()
+  reset_rewinders.call_deferred()
+  unpause_and_clear.call_deferred()
 
 func start_rewind():
+  print_verbose('START start_rewind')
   pause_and_grayout()
-  add_animation_to_library(create_animation())
-  play_backwards('%s/%s' % [LIBRARY_NAME, ANIMATION_NAME])
+  # add_animation_to_library.call_deferred(create_animation())
+  create_animation()
+  play_animation.call_deferred()
+  print_verbose('END start_rewind')
 
 func create_animation() -> Animation:
-  var animation = Animation.new()
+  print_verbose('START create_animation')
+  var animation : Animation
+
+  if get_animation_library(LIBRARY_NAME).has_animation(ANIMATION_NAME):
+    animation = get_animation_library(LIBRARY_NAME).get_animation(ANIMATION_NAME)
+  else:
+    animation = Animation.new()
+    get_animation_library(LIBRARY_NAME).add_animation(ANIMATION_NAME, animation)
+
   animation.length = 0.01
   for rewinder in MechanicallyChallengedRewinder.all():
     if rewinder.max_time_in_seconds > animation.length: animation.length = rewinder.max_time_in_seconds
     rewinder.prepare_rewind(animation)
+  print_verbose('END create_animation')
+  ResourceSaver.save(animation, 'res://animation.tres')
   return animation
+
+func pause_rewinders():
+  for rewinder in MechanicallyChallengedRewinder.all(): rewinder.target.process_mode = ProcessMode.PROCESS_MODE_DISABLED
+
+func unpause_rewinders():
+  for rewinder in MechanicallyChallengedRewinder.all(): rewinder.target.process_mode = ProcessMode.PROCESS_MODE_INHERIT
 
 func reset_rewinders():
   for rewinder in MechanicallyChallengedRewinder.all(): rewinder.reset()
 
 func clear_animation():
+  print_verbose('START clear_animation')
   var library := get_animation_library(LIBRARY_NAME)
   if library.has_animation(ANIMATION_NAME): library.remove_animation(ANIMATION_NAME)
+  print_verbose('END clear_animation')
+
+func play_animation():
+  print_verbose('START play_animation')
+  await get_tree().create_timer(0.1).timeout
+  play('%s/%s' % [LIBRARY_NAME, ANIMATION_NAME])
+  print_verbose('END play_animation')
 
 func add_animation_to_library(animation:Animation):
+  print_verbose('START add_animation_to_library')
   var library := get_animation_library(LIBRARY_NAME)
   if library.has_animation(ANIMATION_NAME): library.remove_animation(ANIMATION_NAME)
   library.add_animation(ANIMATION_NAME, animation)
+  ResourceSaver.save(library, 'res://library.tres')
+  print_verbose('END add_animation_to_library')
--- mechanically-challenged/scripts/rewinder.gd
+++ mechanically-challenged/scripts/rewinder.gd
@@ -18,52 +18,62 @@ var pointer : int
 
 func setup_rewind_track_for(animation:Animation, selector:String=':position', values:Array=positions):
   var target_path := path_to_target(selector)
-  print_verbose('setting up rewind track for %s' % target_path)
+  print_verbose('START setting up rewind track for %s' % target_path)
   var track_index := animation.add_track(Animation.TrackType.TYPE_VALUE)
   animation.track_set_path(track_index, target_path) 
+  for k in animation.track_get_key_count(track_index): animation.track_remove_key(track_index, k)
   var delta_t := 1.0 / resoltion_fps
   var initial_index := pointer - 0
   if initial_index < 0: initial_index = values.size() - 1
+  if initial_index > values.size() - 1: initial_index = 0
   for i in values.size() - 0:
     var idx := (initial_index + i) % values.size()
     var val = values[idx]
     if val == null: continue
     var t = i * delta_t
-    animation.track_insert_key(track_index, t, val)
+    print_verbose('value %s at time %s' % [val, t])
+    animation.track_insert_key(track_index, max_time_in_seconds - t, val)
+  print_verbose('END setting up rewind track for %s' % target_path)
 
 func prepare_rewind(animation:Animation):
   mode = RewinderMode.REWIND
   set_physics_process(mode == RewinderMode.RECORD)
   setup_rewind_track_for(animation, ':position', positions)
-  setup_rewind_track_for(animation, ':skew', skews)
-  setup_rewind_track_for(animation, ':rotation', rotations)
-  setup_rewind_track_for(animation, ':scale', scales)
+  # setup_rewind_track_for(animation, ':skew', skews)
+  # setup_rewind_track_for(animation, ':rotation', rotations)
+  # setup_rewind_track_for(animation, ':scale', scales)
 
 func reset(new_mode:RewinderMode=RewinderMode.RECORD):
-  var number_of_frames := roundi(max_time_in_seconds * resoltion_fps)
-  pointer = 0
-  positions = []
-  scales = []
-  skews = []
-  rotations = []
-  positions.resize(number_of_frames)
-  scales.resize(number_of_frames)
-  skews.resize(number_of_frames)
-  rotations.resize(number_of_frames)
+  print_verbose('START reset')
+  # pointer = 0
+  # positions.fill(null)
+  # scales.fill(null)
+  # skews.fill(null)
+  # rotations.fill(null)
   mode = new_mode
   set_physics_process(mode == RewinderMode.RECORD)
+  print_verbose('END reset')
 
 func _physics_process(_delta: float) -> void:
   if mode == RewinderMode.RECORD:
     record_position()
-    record_skew()
-    record_rotation()
-    record_scale()
+    # record_skew()
+    # record_rotation()
+    # record_scale()
     pointer = (pointer + 1) % positions.size()
 
 func _ready() -> void:
   add_to_group(REWINDER_GROUP)
   target.process_mode = ProcessMode.PROCESS_MODE_PAUSABLE
+  positions = []
+  scales = []
+  skews = []
+  rotations = []
+  var number_of_frames := roundi(max_time_in_seconds * resoltion_fps)
+  positions.resize(number_of_frames)
+  scales.resize(number_of_frames)
+  skews.resize(number_of_frames)
+  rotations.resize(number_of_frames)
   reset()
   MechanicallyChallengedRewinderAnimationPlayer.singleton()
 
@@ -83,7 +93,9 @@ func record_scale():
   scales[pointer % scales.size()] = target.scale
 
 static func all() -> Array[MechanicallyChallengedRewinder]:
+  print_verbose('START all')
   var tree := Engine.get_main_loop() as SceneTree
   var res : Array[MechanicallyChallengedRewinder] = []
   if tree: for r in tree.get_nodes_in_group(REWINDER_GROUP): res.push_back(r)
+  print_verbose('END all')
   return res
